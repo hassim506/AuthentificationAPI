de641cd11f681b6ffb683c78f85ad926fb29efb0
mor tall <tallpape32@gmail.com>
2025-03-18 01:35:33 +0000

    modification route/api

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,3 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-
